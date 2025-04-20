cb72d4a142ecab22303e507fbc886a7d82ec57f0
Tomáš Klepač <74875406+tomasklepac@users.noreply.github.com>
2025-04-20 22:27:42 +0200

    Update Data.cs

--- Models/Data.cs
+++ Models/Data.cs
@@ -1,43 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace project.Models
 {
     /// <summary>
-    /// Trida reprezentujici jeden radek dat
+    /// Represents a single row of data with frequency, temperature, and additional values.
     /// </summary>
     public class Data
     {
         /// <summary>
-        /// Identifikator daneho radku
+        /// Identifier of the data row.
         /// </summary>
         public int Id { get; init; }
-        
+
         /// <summary>
-        /// frekvence pro dany radek
+        /// Frequency value of the row.
         /// </summary>
         public double Frequency { get; init; }
 
         /// <summary>
-        /// teplota pro dany radek
+        /// Temperature value of the row.
         /// </summary>
         public double Temperature { get; init; }
 
         /// <summary>
-        /// 
+        /// Additional values associated with this row, stored as key-value pairs.
         /// </summary>
         public IReadOnlyDictionary<string, double> extraValues { get; init; }
 
         /// <summary>
-        /// Init funkce pro jeden radek dat
+        /// Initializes a new instance of the Data class.
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="freq"></param>
-        /// <param name="temperature"></param>
-        /// <param name="values"></param>
+        /// <param name="id">Unique row identifier.</param>
+        /// <param name="freq">Frequency value.</param>
+        /// <param name="temperature">Temperature value.</param>
+        /// <param name="values">Dictionary of additional values.</param>
         public Data(int id, double freq, double temperature, Dictionary<string, double> values)
         {
             Id = id;
