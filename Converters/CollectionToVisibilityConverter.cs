58136d35a2f8cc196143fda110f5bde55dfe4363
Tomáš Klepač <74875406+tomasklepac@users.noreply.github.com>
2025-04-20 10:44:40 +0200

    Update CollectionToVisibilityConverter.cs

--- Converters/CollectionToVisibilityConverter.cs
+++ Converters/CollectionToVisibilityConverter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -8,15 +8,35 @@ using System.Threading.Tasks;
 
 namespace project.Converters
 {
+    /// <summary>
+    /// Převádí kolekci na bool hodnotu pro ověření, zda je kolekce prázdná nebo ne.
+    /// </summary>
     public class CollectionToVisibilityConverter : IValueConverter
     {
+        /// <summary>
+        /// Převádí kolekci na bool hodnotu pro ověření, zda je kolekce prázdná nebo ne.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             return value is ICollection collection && collection.Count > 0;
-                
+
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        /// <summary>
+        /// Zpětná konverze není podporována.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
             => throw new NotImplementedException();
     }
 }
