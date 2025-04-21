6500eb0ffe06e201900b083deeca5bc97fe73ea8
opatejdl <140671894+Patik04@users.noreply.github.com>
2025-04-21 14:21:02 +0200

    Update MauiProgram.cs
    Uprava konfliktu

--- App/MauiProgram.cs
+++ App/MauiProgram.cs
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls.Hosting;
-using Microsoft.Maui.Hosting;
-using Syncfusion.Maui.Core.Hosting;
-using project.Converters;
-
-namespace project.App;
-
-public static class MauiProgram
-{
-    /// <summary>
-    /// Vytvoří a nakonfiguruje hlavní aplikaci MAUI.
-    /// </summary>
-    public static MauiApp CreateMauiApp()
-    {
-        var builder = MauiApp.CreateBuilder();
-
-        builder
-            .UseMauiApp<App>() // Použití hlavní třídy aplikace
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); // Přidání vlastního fontu
-            })
-            .ConfigureSyncfusionCore(); // Nastavení Syncfusion komponent
-
-
-        return builder.Build(); // Sestavení aplikace
-    }
-}
-=======
 using Syncfusion.Maui.Core.Hosting;
 using LiveChartsCore.SkiaSharpView.Maui;
 using CommunityToolkit.Maui;
@@ -65,4 +34,3 @@ public static class MauiProgram
         return builder.Build();
     }
 }
->>>>>>> export-w-graphs
